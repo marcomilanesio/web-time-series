d49b116db0f75acf9cce2c3038c137a6055050bd
marcomilanesio <milanesio.marco@gmail.com>
2016-11-15 15:53:03 +0100

    add timeout expire for running query
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sparqlclient import SparqlClient
 import re
 import pandas as pd
-
+import json
 
 def convert_date(datestring, month=False):
     if month:
@@ -28,32 +28,46 @@ def extract(l):
     return res
 
 
-q = """
-    SELECT DISTINCT * WHERE
-    {
-      <http://fr.wikipedia.org/wiki/Hillary_Clinton> ?p ?v .
-      OPTIONAL {?v ?p2 ?v2}
-    }
-    ORDER BY ?v
-    """
-url = "http://dbpedia-historique.inria.fr/sparql"
-s = SparqlClient(url)
-res = s.run_query(q)
+def execute_query(url, q, fname):
+    s = SparqlClient(url)
+    res = s.run_query(q)
+    with open(fname, 'w') as outfile:
+        json.dump(res, outfile)
+    return res
 
-p_keys = {}
-for el in res['results']['bindings']:
-    p_key = el['p']['value']
-    if p_key not in p_keys:
-        p_keys[p_key] = [el]
+if __name__ == "__main__":
+    import os
+    import time
+    expire_time = 60 * 60 * 24 * 2  # 2 days
+    datafile = 'hillary_clinton.json'
+    if not os.path.isfile(datafile) or time.time() - os.stat(datafile).st_mtime > expire_time:
+            q = """ SELECT DISTINCT * WHERE
+            {
+              <http://fr.wikipedia.org/wiki/Hillary_Clinton> ?p ?v .
+              OPTIONAL {?v ?p2 ?v2}
+            }
+            ORDER BY ?v
+            """
+            url = "http://dbpedia-historique.inria.fr/sparql"
+            res = execute_query(url, q, datafile)
     else:
-        p_keys[p_key].append(el)
+        with open(datafile) as infile:
+            res = json.load(infile)
 
-regex_keys = ['revPerMonth', 'averageSizePerMonth']
-rev_per_month = [v for k, v in p_keys.items() if re.search('revPerMonth', k)][0]
-size_per_month = [v for k, v in p_keys.items() if re.search('averageSizePerMonth', k)][0]
+    p_keys = {}
+    for el in res['results']['bindings']:
+        p_key = el['p']['value']
+        if p_key not in p_keys:
+            p_keys[p_key] = [el]
+        else:
+            p_keys[p_key].append(el)
 
-rev = extract(rev_per_month)
-size = extract(size_per_month)
+    regex_keys = ['revPerMonth', 'averageSizePerMonth']
+    rev_per_month = [v for k, v in p_keys.items() if re.search('revPerMonth', k)][0]
+    size_per_month = [v for k, v in p_keys.items() if re.search('averageSizePerMonth', k)][0]
 
-print(size)
+    rev = extract(rev_per_month)
+    size = extract(size_per_month)
+
+    print(size)
 
