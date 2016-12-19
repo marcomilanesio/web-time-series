f2587e17a6bf7bb7c7d96b525f60a95f613298c2
marcomilanesio <milanesio.marco@gmail.com>
2016-12-19 11:54:38 +0100

    person details in a class
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

---
+++ person_page.py
@@ -0,0 +1,27 @@
+class PersonPage:
+    def __init__(self, name):
+        self.name = name
+        self.df = None
+        self.important_dates = []
+        self.birth_date = None
+        self.death_date = None
+        self.unique_contributors = None
+
+    def add_birth_date(self, date):
+        self.birth_date = date
+
+    def add_death_date(self, date):
+        self.death_date = date
+
+    def add_important_date(self, date):
+        self.important_dates.append(date)
+
+    def add_unique_contributors(self, num):
+        self.unique_contributors = num
+
+    def __str__(self):
+        s = self.name
+        s += "\nbirth: {}".format(self.birth_date)
+        s += "\ndeath: {}".format(self.death_date)
+        s += "\nunique: {}".format(self.unique_contributors)
+        return s
