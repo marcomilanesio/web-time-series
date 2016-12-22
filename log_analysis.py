b8f0670e7075518c81c865a8a4026d463da50736
marcomilanesio <milanesio.marco@gmail.com>
2016-12-22 16:34:59 +0100

    basic log analysis and pdf/cdf distribution
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

---
+++ log_analysis.py
@@ -0,0 +1,46 @@
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+from scipy.stats.kde import gaussian_kde
+
+
+def compute_stats(lengths):
+    print("Min: {}".format(np.min(lengths)))
+    print("Max: {}".format(np.max(lengths)))
+    print("Mean: {}".format(np.mean(lengths)))
+    print("Std: {}".format(np.std(lengths)))
+    print("Median (50-tile): {}".format(np.percentile(lengths, 50)))   # median.
+    print("95-tile: {}".format(np.percentile(lengths, 95)))
+
+
+def plot_distributions(lengths):
+    figname = './results/distributions.pdf'
+    mu = np.mean(lengths)
+    std = np.std(lengths)
+    cdf = stats.norm.cdf(lengths, mu, std)
+
+    kde = gaussian_kde(lengths)
+    dist_space = np.linspace(min(lengths), max(lengths), 100)
+
+    fig, ax1 = plt.subplots()
+    pdf_line = ax1.plot(dist_space, kde(dist_space), 'b+', label="pdf")
+    ax1.set_xlabel('length')
+
+    ax2 = ax1.twinx()
+    cdf_line = ax2.plot(lengths, cdf, 'r-', label="cdf")
+
+    lines = pdf_line + cdf_line
+    labels = [l.get_label() for l in lines]
+    ax1.legend(lines, labels, loc='best', shadow=True, fancybox=True)
+    plt.savefig(figname)
+    plt.close()
+
+if __name__ == "__main__":
+    logfile = 'timeseries_lengths.txt'
+    with open(logfile, 'r') as infile:
+        arr = infile.readlines()
+        lengths = [int(line.split(":")[1]) for line in arr[1:]]
+        lengths.sort()
+
+    compute_stats(lengths)
+    plot_distributions(lengths)
